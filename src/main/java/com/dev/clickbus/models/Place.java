4cfe276c1ddcd655094503a7b428511ef4f4ff67
Daniel Silva <diielsilvasousa@gmail.com>
2024-06-28 14:26:50 -0300

    chore: creating place model

--- src/main/java/com/dev/clickbus/models/Place.java
+++ src/main/java/com/dev/clickbus/models/Place.java
@@ -1,2 +1,17 @@
-package com.dev.clickbus.models;public class Place {
+package com.dev.clickbus.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public record Place(
+        @Id
+        String id,
+        @Indexed(unique = true)
+        String name,
+        String slug,
+        String city,
+        String state
+) {
 }
