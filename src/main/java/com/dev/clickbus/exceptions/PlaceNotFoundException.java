aa244452314cd7927edf2db20f7c884e57507e61
Daniel Silva <diielsilvasousa@gmail.com>
2024-06-28 16:03:08 -0300

    chore: creating place not found exception

---
+++ src/main/java/com/dev/clickbus/exceptions/PlaceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.dev.clickbus.exceptions;
+
+public class PlaceNotFoundException extends RuntimeException {
+
+    public PlaceNotFoundException(String message) {
+        super(message);
+    }
+
+}
