02a213bb3112c060ed2153ce9fd55d518775f7df
Daniel Silva <diielsilvasousa@gmail.com>
2024-06-28 15:08:39 -0300

    chore: adding model mapper

--- src/main/java/com/dev/clickbus/mappers/ModelMapper.java
+++ src/main/java/com/dev/clickbus/mappers/ModelMapper.java
@@ -1,2 +1,32 @@
-package com.dev.clickbus.mappers;public class ModelMapper {
+package com.dev.clickbus.mappers;
+
+import com.dev.clickbus.dtos.PlaceRequest;
+import com.dev.clickbus.dtos.PlaceResponse;
+import com.dev.clickbus.models.Place;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ModelMapper {
+
+    public Place toModel(PlaceRequest request) {
+        return new Place(
+                request.name(),
+                request.city(),
+                request.state()
+        );
+    }
+
+    public PlaceResponse toResponse(Place place) {
+        return new PlaceResponse(
+                place.getId(),
+                place.getName(),
+                place.getSlug(),
+                place.getCity(),
+                place.getState(),
+                place.getCreatedAt(),
+                place.getUpdatedAt()
+        );
+    }
+
+
 }
