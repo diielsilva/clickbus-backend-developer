e98d3afa20446307da5cd4d683668ced56be91f2
Daniel Silva <diielsilvasousa@gmail.com>
2024-06-28 15:33:19 -0300

    feature: adding create place feature

---
+++ src/main/java/com/dev/clickbus/controllers/PlaceController.java
@@ -0,0 +1,32 @@
+package com.dev.clickbus.controllers;
+
+import com.dev.clickbus.dtos.PlaceRequest;
+import com.dev.clickbus.dtos.PlaceResponse;
+import com.dev.clickbus.services.PlaceService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/places")
+public class PlaceController {
+
+    private final PlaceService service;
+
+    public PlaceController(PlaceService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<PlaceResponse> save(@RequestBody @Valid PlaceRequest request) {
+        PlaceResponse response = service.save(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+
+}
---
+++ src/main/java/com/dev/clickbus/services/PlaceService.java
@@ -0,0 +1,38 @@
+package com.dev.clickbus.services;
+
+import com.dev.clickbus.dtos.PlaceRequest;
+import com.dev.clickbus.dtos.PlaceResponse;
+import com.dev.clickbus.mappers.ModelMapper;
+import com.dev.clickbus.models.Place;
+import com.dev.clickbus.repositories.PlaceRepository;
+import com.dev.clickbus.utils.SlugGenerator;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class PlaceService {
+
+    private final PlaceRepository repository;
+    private final ModelMapper mapper;
+    private final SlugGenerator slugGen;
+
+    public PlaceService(PlaceRepository repository, ModelMapper mapper, SlugGenerator slugGen) {
+        this.repository = repository;
+        this.mapper = mapper;
+        this.slugGen = slugGen;
+    }
+
+    public PlaceResponse save(PlaceRequest request) {
+        Place place = mapper.toModel(request);
+        String slug = slugGen.getByPlaceName(place.getName());
+
+        place.setSlug(slug);
+        place.setCreatedAt(LocalDateTime.now());
+
+        repository.save(place);
+
+        return mapper.toResponse(place);
+    }
+
+}
