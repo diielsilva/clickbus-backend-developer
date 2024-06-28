f22a835204ffc64f3f91fe0dfab156547e99cf34
Daniel Silva <diielsilvasousa@gmail.com>
2024-06-28 15:05:19 -0300

    chore: adding DTOS to place model

--- src/main/java/com/dev/clickbus/dtos/PlaceRequest.java
+++ src/main/java/com/dev/clickbus/dtos/PlaceRequest.java
@@ -1,2 +1,10 @@
-package com.dev.clickbus.dtos;public record PlaceRequest() {
+package com.dev.clickbus.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record PlaceRequest(
+        @NotBlank(message = "Name cannot be null or empty!") String name,
+        @NotBlank(message = "City cannot be null or empty!") String city,
+        @NotBlank(message = "State cannot be null or empty!") String state
+) {
 }
--- src/main/java/com/dev/clickbus/dtos/PlaceResponse.java
+++ src/main/java/com/dev/clickbus/dtos/PlaceResponse.java
@@ -1,2 +1,14 @@
-package com.dev.clickbus.dtos;public record PlaceResponse() {
+package com.dev.clickbus.dtos;
+
+import java.time.LocalDateTime;
+
+public record PlaceResponse(
+        String id,
+        String name,
+        String slug,
+        String city,
+        String state,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
 }
