600e5aebd3337b38bc656747776987abbb03ca1d
Daniel Silva <diielsilvasousa@gmail.com>
2024-07-01 07:35:44 -0300

    fix: fixing context load test

--- src/test/java/com/dev/clickbus/ClickbusApplicationTests.java
+++ src/test/java/com/dev/clickbus/ClickbusApplicationTests.java
@@ -1,13 +1,24 @@
 package com.dev.clickbus;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
 
 @SpringBootTest
 class ClickbusApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @ServiceConnection
+    private static final MongoDBContainer MONGODB_CONTAINER = new MongoDBContainer("mongo:latest");
+
+    @BeforeAll
+    static void setUp() {
+        MONGODB_CONTAINER.start();
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
